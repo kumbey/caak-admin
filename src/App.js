b6fdff96d78715899b15fa0a769f1171c27d9968
purewee790 <purwee790@gmail.com>
2021-09-10 12:37:50 +0800

    avatar_comp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from "react";
 import Login from "./components/Login";
 import TopBar from "./components/TopBar";
 import ForgotPassword from "./components/ForgotPassword";
+import Avatar from "./components/Avatar";
+
 
 function App() {
   return (
     <React.Fragment>
-      <TopBar />
-      <ForgotPassword />
+      <Avatar shadow statusColor={'red'} width={16} height={16}/>
     </React.Fragment>
   );
 }
---
+++ src/components/Avatar.js
@@ -0,0 +1,10 @@
+export default function Avatar(props) {
+    return (
+        <div class="mt-4 flex flex-wrap items-center">
+            <div className={`avatar w-${props.width} h-${props.height} ml-4 ${props.shadow ? "avatar_with-shadow" : ""}`}>
+                <div className={`status bg-${props.statusColor}`}></div>
+                <img src="assets/images/potato.jpg"/>
+            </div>
+        </div>
+    )
+}
