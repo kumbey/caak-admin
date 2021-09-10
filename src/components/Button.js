e771ac653ed90319a8851a1d7074f8cd394e54f7
weahforsage <sanjasapple@gmail.com>
2021-09-10 16:03:53 +0800

    Fixed button classname

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,14 +1,15 @@
 export default function Button(props) {
-    return (
-        <button 
-            type="button" 
-            className={`
-                btn 
-                btn_${props.style} 
-                uppercase
+  return (
+    <button
+      type="button"
+      className={`
+                ${props.className}
+                btn btn_${props.type}
+                ${props.uppercase ? "uppercase" : ""} 
                 ${props.outlined ? "btn_outlined" : ""} 
-            `}>
-                {props.children}
-        </button>
-    )
+            `}
+    >
+      {props.children}
+    </button>
+  );
 }
