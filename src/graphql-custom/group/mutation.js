d8a3b304a2270854bd57f4065b473f14e088db64
kumurbek.b <kumurbek.b@gmail.com>
2021-10-01 15:00:57 +0800

    Group mutation

--- src/graphql-custom/group/mutation.js
+++ src/graphql-custom/group/mutation.js
@@ -1,7 +1,20 @@
 import groupSetField from "./fields/groupSetField";
+import groupIDField from "./fields/groupIDField";
 
 export const createGroup = /* GraphQL */ `
     mutation createGroup($input: CreateGroupInput!) {
         createGroup(input: $input) ${groupSetField}
+    }
+`;
+
+export const updateGroup = /* GraphQL */ `
+    mutation updateGroup($input: UpdateGroupInput!) {
+        updateGroup(input: $input) ${groupSetField}
+    }
+`;
+
+export const deleteGroup = /* GraphQL */ `    
+    mutation deleteGroup($input: DeleteGroupInput!){
+        deleteGroup(input: $input) ${groupIDField}
     }
 `;
--- src/pages/Groups.js
+++ src/pages/Groups.js
@@ -1,35 +1,83 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import {
+  createGroup,
+  deleteGroup,
+  updateGroup,
+} from "../graphql-custom/group/mutation";
+
+import API from "@aws-amplify/api";
+import Auth from "@aws-amplify/auth";
+import { graphqlOperation } from "@aws-amplify/api-graphql";
+import { getGroupList } from "../graphql-custom/group/queries";
+import { getCategoryID } from "../graphql-custom/category/queries";
+
+import { convertDateTime } from "../components/utils";
+import { useToast } from "../components/Toast/ToastProvider";
+
 import Tables from "../components/Tables";
 import Input from "../components/Input";
 import Modal from "../components/Modal";
 import Button from "../components/Button";
-import {getGroupList} from "../graphql-custom/group/queries";
-import {graphqlOperation} from "@aws-amplify/api-graphql";
-import {getCategoryID} from "../graphql-custom/category/queries";
-import API from "@aws-amplify/api";
-import {convertDateTime} from "../components/utils";
-import {useToast} from "../components/Toast/ToastProvider";
 import Select from "../components/Select";
 import TextArea from "../components/TextArea";
-import Dropzone from "../components/Dropzone";
-import Auth from "@aws-amplify/auth";
-import {ApiFileUpload} from "../utility/ApiHelper";
-import {createGroup} from "../graphql-custom/group/mutation";
+import DropZone from "../components/Dropzone";
+import { ApiFileUpload } from "../utility/ApiHelper";
 
 const Groups = () => {
+  const { addToast } = useToast();
   const [isShowModal, setShowModal] = useState(false);
   const [groupName, setGroupName] = useState("");
   const [groupAbout, setGroupAbout] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-  const {addToast} = useToast();
   const [groups, setGroups] = useState([]);
   const [catID, setCatID] = useState([]);
   const [selectedCatID, setSelectedCatID] = useState("");
-  const [coverImage, setCoverImage] = useState();
-  const [profileImage, setProfileImage] = useState();
+  const [isShowEdit, setIsShowEdit] = useState(false);
+  const [currentEditingData, setCurrentEditingData] = useState();
+  const [inputError, setInputError] = useState(false);
+  const [coverImage, setCoverImage] = useState({});
+  const [profileImage, setProfileImage] = useState({});
 
   const toggleModal = () => {
     setShowModal(!isShowModal);
+  };
+  const editGroupModal = async (item) => {
+    setIsShowEdit(true);
+    setCurrentEditingData(item);
+  };
+
+  const editGroupFunction = async (event) => {
+    event.preventDefault();
+    try {
+      await API.graphql({
+        query: updateGroup,
+        variables: {
+          input: {
+            id: currentEditingData.id,
+            icon: currentEditingData.icon,
+            name: currentEditingData.name,
+          },
+        },
+      }).then(() => {
+        addToast({ content: `${currentEditingData.name}`, title: "Амжилттай" });
+      });
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  const deleteGroupFunction = async (id) => {
+    if (window.confirm("Та устгахдаа итгэлтэй байна уу?"))
+      try {
+        await API.graphql({
+          query: deleteGroup,
+          variables: { input: { id: id } },
+        }).then(() => {
+          addToast({ content: `Устгалаа`, title: "Амжилттай" });
+        });
+      } catch (ex) {
+        console.log(ex);
+      }
   };
 
   const onSubmit = async (event) => {
@@ -62,15 +110,12 @@ const Groups = () => {
           setIsLoading(false);
           setGroupName(null);
           setGroupAbout(null);
-          console.log(
-              `Successfully added ${result.data.createGroup.name} to groups`
-          );
         });
       } catch (ex) {
         console.log(ex);
       }
     } else {
-      alert("Аль нэг талбар хоосон байна");
+      setInputError(true);
     }
   };
 
@@ -84,96 +129,163 @@ const Groups = () => {
   }, []);
 
   return (
-      <div className="flex flex-col w-screen h-screen font-sans workspace">
-        <div className="">
-          <div className="mb-4">
-            <h1>Группууд</h1>
-            <div className="flex mt-4">
-              <Button className="bg-primary-400" onClick={() => toggleModal()}>
-                Шинэ групп үүсгэх
-              </Button>
-            </div>
+    <div className="flex flex-col w-screen h-screen font-sans workspace">
+      <div className="">
+        <div className="mb-4">
+          <h1>Группууд</h1>
+          <div className="flex mt-4">
+            <Button className="bg-primary-400" onClick={() => toggleModal()}>
+              Шинэ групп үүсгэх
+            </Button>
           </div>
-          <div className="mb-4">
-            <Tables styles="striped" fullWidth="w-full">
-              {groups.map((group) => {
-                return (
-                    <tr key={group.id}>
-                      <td>{group.name}</td>
-                      <td>{group.icon}</td>
-
-                      <td>{convertDateTime(group.createdAt)}</td>
-                      <td>{`${
-                          group.createdAt !== group.updatedAt
-                              ? convertDateTime(group.createdAt)
-                              : "Засвар ороогүй"
-                      }`}</td>
-                      <td>
-                        <a href="#edit">
-                          <i className="text-2xl las la-edit"/>
-                        </a>
-                        <a href="#del">
-                          <i className="ml-4 text-2xl las la-trash-alt"/>
-                        </a>
-                      </td>
-                    </tr>
-                );
-              })}
-            </Tables>
+        </div>
+      </div>
+      <div className="mb-4">
+        <Tables styles="striped" fullWidth="w-full">
+          {currentEditingData && (
             <Modal
-                show={isShowModal}
-                title="Шинэ групп үүсгэх"
-                content="content"
-                onClose={() => setShowModal(false)}
-                type="submit"
-                onSubmit={onSubmit}
-                loading={isLoading}
-                submitBtnName="Шинэ групп нэмэх"
+              modalType={"centered"}
+              show={isShowEdit}
+              title={`Засвар оруулах`}
+              onClose={() => setIsShowEdit(false)}
+              type="submit"
+              onSubmit={editGroupFunction}
+              loading={isLoading}
+              submitBtnName="Хадгалах"
+              content={currentEditingData}
             >
               <div className="mt-8 max-w-md">
                 <div className="grid grid-cols-1 gap-6">
                   <Input
-                      value={groupName}
-                      onChange={(e) => setGroupName(e.target.value)}
-                      title="Групп нэр"
+                    value={currentEditingData.name || ""}
+                    onChange={(e) =>
+                      setCurrentEditingData({
+                        ...currentEditingData,
+                        name: e.target.value,
+                      })
+                    }
+                    title="Групп нэр"
+                    error={inputError}
+                    errorMessage={`${
+                      !currentEditingData ? "Групп нэрийг оруулна уу" : ""
+                    }`}
                   />
-                  {/* <Input
-                  value={groupIconName}
-                  onChange={(e) => setGroupIconName(e.target.value)}
-                  label="Icon нэр"
-                /> */}
-
                   <Select
-                      title="Категори сонгох"
-                      onChange={(e) => setSelectedCatID(e.target.value)}
+                    title="Категори сонгох"
+                    onChange={(e) => setSelectedCatID(e.target.value)}
                   >
-                    <option hidden>Сонгох...</option>
+                    <option disabled selected>
+                      Сонгох...
+                    </option>
                     {catID.map((cat) => {
-                      return (
-                          <option key={cat.id} value={cat.id}>
-                            {cat.name}
-                          </option>
-                      );
+                      return <option value={cat.id}>{cat.name}</option>;
                     })}
                   </Select>
-
                   <TextArea
-                      name="about"
-                      title="Тухай"
-                      row="4"
-                      onChange={(e) => setGroupAbout(e.target.value)}
-                      value={groupAbout}
+                    name="about"
+                    title="Тухай"
+                    row="4"
+                    onChange={(e) => setGroupAbout(e.target.value)}
+                    value={groupAbout}
                   />
-                  <h4>Cover image upload</h4>
-                  <Dropzone title={"Drop it here"} onUpload={setCoverImage}/>
-                  <h4>Profile image upload</h4>
-                  <Dropzone title={"Drop it here"} onUpload={setProfileImage}/>
+                  {/* <Input
+                      value={currentEditingData.icon || ""}
+                      onChange={(e) =>
+                        setCurrentEditingData({
+                          ...currentEditingData,
+                          icon: e.target.value,
+                        })
+                      }
+                      label="Категори Icon нэр"
+                      error={inputError}
+                      errorMessage={`${
+                        !currentEditingData
+                          ? "Категорийн айкон нэрийг оруулна уу"
+                          : ""
+                      }`}
+                    /> */}
                 </div>
               </div>
             </Modal>
+          )}
+          {groups.map((group) => {
+            return (
+              <tr key={group.id}>
+                <td>{group.name}</td>
+                <td>{group.icon}</td>
+
+                <td>{convertDateTime(group.createdAt)}</td>
+                <td>{`${
+                  group.createdAt !== group.updatedAt
+                    ? convertDateTime(group.createdAt)
+                    : "Засвар ороогүй"
+                }`}</td>
+                <td>
+                  <span
+                    onClick={() => editGroupModal(group)}
+                    className={"cursor-pointer"}
+                  >
+                    <i className="las la-edit text-2xl " />
+                  </span>
+                  <span
+                    onClick={() => deleteGroupFunction(group.id)}
+                    className={"cursor-pointer"}
+                  >
+                    <i className="las la-trash-alt text-2xl ml-4" />
+                  </span>
+                </td>
+              </tr>
+            );
+          })}
+        </Tables>
+        <Modal
+          show={isShowModal}
+          title="Шинэ групп үүсгэх"
+          content="content"
+          onClose={() => setShowModal(false)}
+          type="submit"
+          onSubmit={onSubmit}
+          loading={isLoading}
+          submitBtnName="Шинэ групп нэмэх"
+        >
+          <div className="mt-8 max-w-md">
+            <div className="grid grid-cols-1 gap-6">
+              <Input
+                value={groupName}
+                onChange={(e) => setGroupName(e.target.value)}
+                title="Групп нэр"
+              />
+
+              <Select
+                title="Категори сонгох"
+                onChange={(e) => setSelectedCatID(e.target.value)}
+              >
+                <option hidden>Сонгох...</option>
+                {catID.map((cat) => {
+                  return (
+                    <option key={cat.id} value={cat.id}>
+                      {cat.name}
+                    </option>
+                  );
+                })}
+              </Select>
+
+              <TextArea
+                name="about"
+                title="Тухай"
+                row="4"
+                onChange={(e) => setGroupAbout(e.target.value)}
+                value={groupAbout}
+              />
+              <h4>Cover image upload</h4>
+              <DropZone title={"Drop it here"} onUpload={setCoverImage} />
+              <h4>Profile image upload</h4>
+              <DropZone title={"Drop it here"} onUpload={setProfileImage} />
+            </div>
           </div>
-        </div>
+        </Modal>
       </div>
+    </div>
   );
 };
 
